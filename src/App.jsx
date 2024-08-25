fc647f267b45453fc9a43e6be524a7538069ef1c
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-08-25 15:10:59 +0200

    step 3

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,17 @@ import ContactList from "./components/ContactList/ContactList";
 import initialContacts from "./components/contacts.json";
 import { useState } from "react";
 import SearchBox from "./components/SearchBox/SearchBox";
+import ContactForm from "./components/ContactForm/ContactForm";
 
 export default function App() {
   const [contacts, setContacts] = useState(initialContacts);
   const [filter, setFilter] = useState("");
+
+  const addContact = (newContact) => {
+    setContacts((prevContact) => {
+      return [...prevContact, newContact];
+    });
+  };
 
   const visibleContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLocaleLowerCase())
@@ -13,6 +20,7 @@ export default function App() {
   return (
     <>
       <h1>Phonebook</h1>
+      <ContactForm onAdd={addContact} />
       <SearchBox value={filter} onFilter={setFilter} />
       <ContactList contacts={visibleContacts} />
     </>
