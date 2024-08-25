33c71cca8a0cc0c4c2bd0829520b8078387fcb54
Nataliia Hordiienko <Natalihordienko@gmail.com>
2024-08-25 10:36:57 +0200

    fixed step 1

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -2,17 +2,17 @@ import css from "./Contact.module.css";
 import { FaUser } from "react-icons/fa6";
 import { FaPhoneAlt } from "react-icons/fa";
 
-export default function Contact({ name, number }) {
+export default function Contact({ data: { name, number } }) {
   return (
     <>
       <div className={css.contactItem}>
         <div className={css.contact}>
           <div className={css.userName}>
-            <FaUser size="20" />
+            <FaUser size="25" />
             <p>{name}</p>
           </div>
           <div className={css.userPhone}>
-            <FaPhoneAlt size="20" />
+            <FaPhoneAlt size="25" />
             <p>{number}</p>
           </div>
         </div>
